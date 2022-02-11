8b5742d1987628ab87746cc9b80a745499a7ecb4
Joseph Diza <josephm.diza@gmail.com>
2022-02-11 11:44:57 -0800

    Build initial UI with slint_build

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::{Arg, App};
 
+use slint::{ModelRc, VecModel, SharedString};
+slint::include_modules!();
+
+
 fn main() {
     let matches = App::new("Anno")
         .version("0.1.0")
@@ -7,5 +11,10 @@ fn main() {
         .about("Easily create beautiful, customizable annotations for pdfs")
         .arg(Arg::new("fp").help("File path to the pdf"))
         .get_matches();
+
+    let fp = matches.value_of("fp").unwrap_or("");
+
+    let ui = AppWindow::new();
+    ui.run();
 }
 
