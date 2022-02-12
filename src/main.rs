29c959f673fe259168b748235f456c8e04f53e13
Joseph Diza <josephm.diza@gmail.com>
2022-02-11 22:35:06 -0800

    Add poppler-rs and get initial text from pdf

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::{Arg, App};
-
+use poppler::{PopplerDocument, PopplerPage};
 use slint::{ModelRc, VecModel, SharedString};
+use std::rc::Rc;
 slint::include_modules!();
-
 
 fn main() {
     let matches = App::new("Anno")
@@ -13,8 +13,17 @@ fn main() {
         .get_matches();
 
     let fp = matches.value_of("fp").unwrap_or("");
-
+    let file: PopplerDocument = PopplerDocument::new_from_file(fp, "").unwrap();
+    let page: PopplerPage = file.get_page(0).unwrap();
+    
+    let text = page.get_text().unwrap_or("");
+    // Display text in ui here
+    //let pdf_model = Rc::new(PDFData { content: SharedString::from(text.to_owned())});
+    let pdf_model = PDFData { content: SharedString::from(text.to_owned())};
     let ui = AppWindow::new();
+
+    ui.set_pdf(pdf_model);
+    let _appwin_weak = ui.as_weak();
     ui.run();
 }
 
