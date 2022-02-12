4c4f1272013abbee7eb12611864aabe2a2042603
Joseph Diza <josephm.diza@gmail.com>
2022-02-12 02:48:12 -0800

    Render pdf with cairo-rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+//extern crate clap;
+//extern crate poppler;
+//extern crate slint;
+extern crate cairo;
 use clap::{Arg, App};
 use poppler::{PopplerDocument, PopplerPage};
 use slint::{ModelRc, VecModel, SharedString};
-use std::rc::Rc;
 slint::include_modules!();
+use cairo::{ ImageSurface, Format, Context };
+use std::rc::Rc;
+use std::fs::File
 
 fn main() {
     let matches = App::new("Anno")
@@ -14,12 +20,30 @@ fn main() {
 
     let fp = matches.value_of("fp").unwrap_or("");
     let file: PopplerDocument = PopplerDocument::new_from_file(fp, "").unwrap();
-    let page: PopplerPage = file.get_page(0).unwrap();
+    
+:   let page: PopplerPage = file.get_page(0).unwrap();
     
     let text = page.get_text().unwrap_or("");
     // Display text in ui here
     //let pdf_model = Rc::new(PDFData { content: SharedString::from(text.to_owned())});
     let pdf_model = PDFData { content: SharedString::from(text.to_owned())};
+
+    /*
+    let pdf_model = PDFData {
+        content: SharedString::from(text.to_owned()),
+        source: "output.png"
+    };
+
+    // Create Cairo surface
+    //let surface = ImageSurface::create(Format::ARgb32, 3200, 1800).expect(“Couldn’t create surface”);
+    let context = page.render();
+    context.paint();
+    let mut file = File::create("output.png").expect("Couldn’t create file"); 
+    context.write_to_png(&mut file).expect("Couldn’t write to png");
+    //surface.write_to_png(&mut file).expect("Couldn’t write to png");
+
+    */
+
     let ui = AppWindow::new();
 
     ui.set_pdf(pdf_model);
