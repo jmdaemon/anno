9a22c13d012f6805ad3b55e32b44ff0951bcd0ba
Joseph Diza <josephm.diza@gmail.com>
2022-02-19 22:43:31 -0800

    Remove SLINT-UI project dependencies and related UI code
    There isn't really any support for Slint to render any SVG graphics to
    the screen, and there isn't much support for doing things such as
    rendering PDF files. Additionally in the last commit the UI toolkit was
    not able to display the full text of the PDF file.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-//extern crate clap;
-//extern crate poppler;
-//extern crate slint;
-extern crate cairo;
-use clap::{Arg, App};
 use poppler::{PopplerDocument, PopplerPage};
-use slint::{ModelRc, VecModel, SharedString};
-slint::include_modules!();
-use cairo::{ ImageSurface, Format, Context };
-use std::rc::Rc;
-use std::fs::File
+use clap::{Arg, App};
 
 fn main() {
     let matches = App::new("Anno")
@@ -21,33 +12,8 @@ fn main() {
     let fp = matches.value_of("fp").unwrap_or("");
     let file: PopplerDocument = PopplerDocument::new_from_file(fp, "").unwrap();
     
-:   let page: PopplerPage = file.get_page(0).unwrap();
-    
+    // Get the first page
+    let page: PopplerPage = file.get_page(0).unwrap();
     let text = page.get_text().unwrap_or("");
-    // Display text in ui here
-    //let pdf_model = Rc::new(PDFData { content: SharedString::from(text.to_owned())});
-    let pdf_model = PDFData { content: SharedString::from(text.to_owned())};
-
-    /*
-    let pdf_model = PDFData {
-        content: SharedString::from(text.to_owned()),
-        source: "output.png"
-    };
-
-    // Create Cairo surface
-    //let surface = ImageSurface::create(Format::ARgb32, 3200, 1800).expect(“Couldn’t create surface”);
-    let context = page.render();
-    context.paint();
-    let mut file = File::create("output.png").expect("Couldn’t create file"); 
-    context.write_to_png(&mut file).expect("Couldn’t write to png");
-    //surface.write_to_png(&mut file).expect("Couldn’t write to png");
-
-    */
-
-    let ui = AppWindow::new();
-
-    ui.set_pdf(pdf_model);
-    let _appwin_weak = ui.as_weak();
-    ui.run();
 }
 
